710518d44c5be68e19140026038758615acafdb5
Ufuk Kaan GÜVEN <ufukkaan19@gmail.com>
2023-07-11 14:48:42 +0300

    small fix in yildizlarIleUcgenYapanProgram

--- Java/javaCohortsHomeworks/yildizlarIleUcgenYapanProgram/src/Main.java
+++ Java/javaCohortsHomeworks/yildizlarIleUcgenYapanProgram/src/Main.java
@@ -1,21 +1,48 @@
 import java.util.Scanner;
 
 /*
-Java'da döngüler kullanılarak yıldızlar ile üçgen yapıyoruz.
+Java'da döngüler kullanarak yıldızlar ile elmas yapınız.
 
 
  */
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Üçgenin satır sayısını girin: ");
-        int satirSayisi = scanner.nextInt();
+        System.out.print("Elmasın yüksekliği (tek sayı): ");
+        int yukseklik = scanner.nextInt();
 
-        for (int i = 1; i <= satirSayisi; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
+        int bosluk = yukseklik / 2;
+        int yildiz = 1;
+
+        // Elmasın üst kısmını çizdirme
+        for (int i = 0; i < yukseklik / 2 + 1; i++) {
+            for (int j = 0; j < bosluk; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < yildiz; j++) {
+                System.out.print("*");
             }
             System.out.println();
+
+            bosluk--;
+            yildiz += 2;
+        }
+
+        // Elmasın alt kısmını çizdirme
+        bosluk = 1;
+        yildiz = yukseklik - 2;
+
+        for (int i = 0; i < yukseklik / 2; i++) {
+            for (int j = 0; j < bosluk; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < yildiz; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+            bosluk++;
+            yildiz -= 2;
         }
     }
 }
