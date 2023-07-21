e8274fdae04138a9ef3590eb7134ed0f8aeced59
Ufuk Kaan GÜVEN <ufukkaan19@gmail.com>
2023-07-22 00:35:40 +0300

    done Grade, GradeTest

--- Java/hw3/src/Main.java
+++ Java/hw3/src/Main.java
@@ -1,6 +1,10 @@
+import util.Grade;
+import util.GradeTest;
 
 public class Main {
     public static void main(String[] args) {
+
+        GradeTest.values();
 
     }
 }
--- Java/hw3/src/util/GradeTest.java
+++ Java/hw3/src/util/GradeTest.java
@@ -1,4 +1,8 @@
 package util;
-
 public class GradeTest {
-}
+    public static void values() {
+        for (Grade grade : Grade.values()) {
+            System.out.println(grade);
+        }
+    }
+    }
